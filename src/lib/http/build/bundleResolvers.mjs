b58be5b6a59069506d3ac713b8f4a6bd939634a3
Ant Stanley <antstanley@users.noreply.github.com>
2019-05-15 07:16:05 +0100

    bump dependencies, fix error handling in dev

--- src/lib/http/build/bundleResolvers.mjs
+++ src/lib/http/build/bundleResolvers.mjs
@@ -4,31 +4,36 @@ import { logger, rollupDefault } from '../../utils'
 
 async function build (inputOptions, outputOptions, outputLoc) {
   // create a bundle
-  const bundle = await rollup(inputOptions)
-  // generate code
-  const { code } = await bundle.generate(outputOptions)
-  // logger(`warn`, `code: ${code}`)
-  writeFileSync(outputLoc, code)
-  logger('info', `dev: http server ready!`)
+  try {
+    const bundle = await rollup(inputOptions)
+    const { code } = await bundle.generate(outputOptions)
+    writeFileSync(outputLoc, code)
+    logger('info', `dev: http server ready!`)
+  } catch (error) {
+    logger('error', `dev: function bundle failed with error: ${error}`)
+  }
   return true
 }
 
 export default async (resolver, outputLoc, rollupConfig) => {
   //  console.log(output)
   logger('info', `dev: Generating bundled resolver ...`)
+  try {
+    const input = `${resolver}/index.mjs`
+    // bundle
+    if (existsSync(resolver)) {
+      const inputOptions = await rollupDefault(rollupConfig, input)
 
-  const input = `${resolver}/index.mjs`
-  // bundle
-  if (existsSync(resolver)) {
-    const inputOptions = await rollupDefault(rollupConfig, input)
+      const outputOptions = {
+        format: 'cjs'
+      }
 
-    const outputOptions = {
-      format: 'cjs'
+      return build(inputOptions, outputOptions, outputLoc)
+    } else {
+      logger('warn', `Path to ${resolver} doesn't exit`)
+      return null
     }
-
-    return build(inputOptions, outputOptions, outputLoc)
-  } else {
-    logger('warn', `Path to ${resolver} doesn't exit`)
-    return null
+  } catch (error) {
+    logger('error', `dev: Unable to bundle resolver with error. ${error}`)
   }
 }
